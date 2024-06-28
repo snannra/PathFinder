0ebef43beb7178a77ed3d3e59f02394051c654a5
Sohan Nannra <49798755+snannra@users.noreply.github.com>
2024-06-27 22:37:02 -0700

    set starting form

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,48 @@
 import Button from "./components/Button";
 import Form from "./components/Form";
+import "./App.css"; // Import the CSS file
+import { useState } from "react";
 
 function App() {
+  const [startingAdress, setStartingAdress] = useState("");
+  const [endingAdress, setEndingAdress] = useState("");
+  const [milesRan, setMilesRan] = useState("");
+
+  const handleSubmit = () => {
+    console.log("Starting Adress: ", startingAdress);
+    console.log("Ending Adress: ", endingAdress);
+    console.log("Miles Ran: ", milesRan);
+  };
+
   return (
-    <>
-      <Form label="Starting Address" placeholder="1234 Berry lane" />
-      <Form label="Ending Address" placeholder="1234 Berry lane" />
-      <Button
-        children="Submit"
-        color="secondary"
-        onClick={() => console.log("clicked")}
-      />
-    </>
+    <div className="app-container">
+      <h1 className="title">Path Finder</h1>
+      <div className="form-container">
+        <Form
+          label="Starting Address"
+          placeholder="1234 Berry Lane"
+          onChange={setStartingAdress}
+        />
+        <Form
+          label="Ending Address"
+          placeholder="1234 Berry Lane"
+          onChange={setEndingAdress}
+        />
+        <Form
+          label="How Many Miles Do You Want To Run?"
+          placeholder="e.g. 5"
+          onChange={setMilesRan}
+        />
+        <div className="button-wrapper">
+          <Button
+            children="Submit"
+            color="secondary"
+            onClick={handleSubmit}
+          />
+        </div>
+      </div>
+      <hr className="separator" />
+    </div>
   );
 }
 
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,21 +1,26 @@
-import "./Form.css";
-
 interface Props {
   label: string;
   placeholder: string;
+  onChange: (value: string) => void;
 }
 
-const Form = ({ label, placeholder }: Props) => {
+const Form = ({ label, placeholder, onChange }: Props) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.value);
+  };
+
   return (
     <div className="mb-3">
       <label htmlFor="exampleFormControlInput1" className="form-label">
         {label}
       </label>
       <input
+        width={200}
         type="email"
         className="form-control"
         id="exampleFormControlInput1"
         placeholder={placeholder}
+        onChange={handleChange}
       />
     </div>
   );
